0a771417044890d5e4f54de95d006705d9f64cca
tavesjao <jptavesaraujo@hotmail.com>
2023-06-20 16:01:13 -0300

    refactor: fix methods in transformer.py

--- src/Transform/Transformer.py
+++ src/Transform/Transformer.py
@@ -9,6 +9,9 @@ class Transformer:
         self.file_name = file_name
         self.df = pd.read_json(f'../data/{self.file_name}.json')
 
+    def print_df(self):
+        print(self.df.head())
+
     def remove_stopwords(self, text):
         stopwords_pt = set(stopwords.words('portuguese'))
         tokens = text.split()
@@ -20,20 +23,38 @@ class Transformer:
 
     def remove_line_breaks(self, text):
         return re.sub(r'\n', '', text)
+    
+    def remove_duplicates(self):
+        self.df.drop_duplicates(subset=('link'), ignore_index=True, inplace=True)
+
+        return self.df
+
+    def remove_date(self):
+        self.df.drop(columns=['date'], inplace=True)
         
-    def clean_text(self, remove_date=False, remove_dupes=False):
+        return self.df
+        
+    def clean_text(self, save: bool = False):
         df_ct = self.df.copy()
 
-        if remove_date:
-            df_ct.drop_duplicates(subset=('link'), ignore_index=True, inplace=True)
-            df_ct.drop(columns=['date'], inplace=True)
+        #check if there is a date column
+        if 'date' in df_ct.columns:
+            self.remove_date()
 
+        #check if there are duplicates
+
+        if df_ct['link'].duplicated().any():
+            self.remove_duplicates()
+    
         df_ct['text'] = df_ct['text'].apply(self.remove_line_breaks)
         df_ct['text'] = df_ct['text'].apply(self.remove_punctuation)
         df_ct['text'] = df_ct['text'].apply(self.remove_stopwords)
-        return df_ct['text'].tolist()
-    
-    def save_clean_text(self, remove_date=False, remove_dupes=False):
-        with open(f'../clean_data/{self.file_name}_clean.txt', 'w') as f:
-            for text in self.clean_text():
-                f.write(text + '\n')
+
+        #return to save df as json
+        
+        if save:
+            #use utf-8 encoding to avoid problems with special characters
+            df_ct.to_json(f'../clean_data/{self.file_name}_cleaned.json', orient='records', force_ascii=False)
+            print(f'File {self.file_name}_cleaned.json saved successfully!')
+
+        return df_ct
